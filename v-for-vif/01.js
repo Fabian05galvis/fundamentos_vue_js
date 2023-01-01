08844b42b5a97784f42d1a7a1e61f8e57853338d
WilmerGalvis <dirdesarrollo@zencillo.com>
2023-01-01 17:11:54 -0500

    first commit

---
+++ v-for-vif/01.js
@@ -0,0 +1,24 @@
+//instanciamos vue js
+/*const app = new Vue({
+    el: '#app',
+    data : {
+        title: 'Hello worlod con vue js'
+    }
+})*/
+
+//instanciamo vue js 3.0
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        title: 'Hello Vue!',
+        frutas : ["manzana","pera","platano"],
+        frutas_objets : [
+            {name:'Pera',count:10},
+            {name:'Manzana',count:0},
+            {name:'Banana',count:155},
+        ]
+      }
+    }
+  }).mount('#app')
